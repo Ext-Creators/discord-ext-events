b5cc1c86ed2ab28ccad9686c1cf702aa4bbbd7a4
Riley Shaw <contact@shiney.dev>
2020-07-30 15:43:21 +0100

    josh you said you changed this

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
